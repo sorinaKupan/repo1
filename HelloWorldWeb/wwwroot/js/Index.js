7f775da50340bd4fd4e301ce592a06ecad5e0985
AzureAD\SorinaKupan <sorina.kupan@principal33.com>
2021-08-03 10:35:13 +0300

    Javascript folder for index

---
+++ HelloWorldWeb/wwwroot/js/Index.js
@@ -0,0 +1,16 @@
+// This JS file now uses jQuery. Pls see here: https://jquery.com/
+$(document).ready(function () {
+    // see https://api.jquery.com/click/
+    $("#createButton").click(function () {
+        var newcomerName = $("#nameField").val();
+
+
+
+        // Remember string interpolation
+        $("#list").append(`<li>${newcomerName}</li>`);
+
+
+
+        $("#nameField").val("");
+    })
+});
