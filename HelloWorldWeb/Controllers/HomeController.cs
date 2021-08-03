abd549c4a5c97d8490601bbfb077aa9d53f21562
AzureAD\SorinaKupan <sorina.kupan@principal33.com>
2021-08-03 11:45:10 +0300

    The data is taken from the back-end & model TeamInfo has been created

--- HelloWorldWeb/Controllers/HomeController.cs
+++ HelloWorldWeb/Controllers/HomeController.cs
@@ -12,15 +12,24 @@ namespace HelloWorldWeb.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly TeamInfo teamInfo;
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+            teamInfo = new TeamInfo();
+            teamInfo.Name ="Team2";
+            teamInfo.TeamMembers = new List<string>();
+            teamInfo.TeamMembers.Add("Gabriel");
+            teamInfo.TeamMembers.Add("Delia");
+            teamInfo.TeamMembers.Add("Sorina");
+            teamInfo.TeamMembers.Add("Rares");
+            teamInfo.TeamMembers.Add("Catalin");
         }
 
         public IActionResult Index()
         {
-            return View();
+            return View(teamInfo);
         }
 
         public IActionResult Privacy()
