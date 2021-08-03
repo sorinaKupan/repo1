158f5a7ada24d4c7474464f284b7f0ca61678bfc
AzureAD\SorinaKupan <sorina.kupan@principal33.com>
2021-08-03 10:15:31 +0300

    Add team member

--- HelloWorldWeb/wwwroot/js/site.js
+++ HelloWorldWeb/wwwroot/js/site.js
@@ -2,3 +2,19 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+// This JS file now uses jQuery. Pls see here: https://jquery.com/
+$(document).ready(function () {
+    // see https://api.jquery.com/click/
+    $("#createButton").click(function () {
+        var newcomerName = $("#nameField").val();
+
+
+
+        // Remember string interpolation
+        $("#list").append(`<li>${newcomerName}</li>`);
+
+
+
+        $("#nameField").val("");
+    })
+});
