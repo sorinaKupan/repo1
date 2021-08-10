e49cf51070d00d296119aa733c4ab152fbfb0ca8
AzureAD\SorinaKupan <sorina.kupan@principal33.com>
2021-08-10 09:31:30 +0300

    Added Skill class

---
+++ HelloWorldWeb/Models/Skill.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloWorldWeb.Models
+{
+    public class Skill
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string SkillUrl { get; set; }
+    }
+}
