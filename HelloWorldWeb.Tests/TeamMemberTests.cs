4e91ae861db9169502035f2b3efd476b5cf492ec
AzureAD\SorinaKupan <sorina.kupan@principal33.com>
2021-08-12 09:39:53 +0300

    Setup test TestGetAgeEqual using timeMock

--- HelloWorldWeb.Tests/TeamMemberTests.cs
+++ HelloWorldWeb.Tests/TeamMemberTests.cs
@@ -12,58 +12,21 @@ namespace HelloWorldWeb.Tests
 {
     public class TeamMemberTests
     {
-        private ITimeService timeService;
+        private Mock<ITimeService> timeMock;
 
-        public TeamMemberTests()
+        private void InitializeTimeServiceMock()
         {
-            var mock = new Mock<ITimeService>();
-            mock.Setup(_ => _.Now()).Returns(new DateTime(2021, 08, 11));
-            timeService = mock.Object;
-        }
-
-        [Fact]
-        public void TestEqualsIdFalse()
-        {
-            // Assume
-
-            // Act
-            TeamMember member1 = new TeamMember("Tudor", timeService);
-            TeamMember member2 = new TeamMember("Tudor", timeService);
-
-            // Assert
-            Assert.False(member1.Equals(member2));
-        }
-
-        [Fact]
-        public void TestEqualsNameFalse()
-        {
-            // Assume
-
-            // Act
-            TeamMember member1 = new TeamMember("Tudor", timeService);
-            TeamMember member2 = new TeamMember("Cioara", timeService);
-
-            // Assert
-            Assert.False(member1.Equals(member2));
-        }
-
-        [Fact]
-        public void TestIdCounterIncrement()
-        {
-            // Assume
-            int idCounter = TeamMember.GetIdCounter();
-
-            // Act
-            TeamMember member1 = new TeamMember("Tudor", timeService);
-
-            // Assert
-            Assert.Equal(idCounter + 1, TeamMember.GetIdCounter());
+            timeMock = new Mock<ITimeService>();
+            timeMock.Setup(_ => _.Now()).Returns(new DateTime(2021, 08, 11));
+            
         }
 
         [Fact]
         public void TestGetAgeEqual()
         {
             // Assume
+            InitializeTimeServiceMock();
+            var timeService = timeMock.Object;
             TeamMember teamMember = new TeamMember("Ioan", timeService);
             teamMember.BirthDate = new DateTime(2000, 01, 01);
             int expectedAge = 21;
@@ -72,22 +35,8 @@ namespace HelloWorldWeb.Tests
             int computedAge = teamMember.getAge();
 
             // Assert
+            timeMock.Verify(_ => _.Now(), Times.AtMostOnce());
             Assert.Equal(expectedAge, computedAge);
-        }
-
-        [Fact]
-        public void TestGetAgeNotEqual()
-        {
-            // Assume
-            TeamMember teamMember = new TeamMember("Ioan", timeService);
-            teamMember.BirthDate = new DateTime(2000, 01, 01);
-            int expectedAge = 1;
-
-            // Act
-            int computedAge = teamMember.getAge();
-
-            // Assert
-            Assert.NotEqual(expectedAge, computedAge);
         }
     }
 }
