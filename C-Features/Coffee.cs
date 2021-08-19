ce1c387b340f3fc6949fd56115df806c74a63286
AzureAD\SorinaKupan <sorina.kupan@principal33.com>
2021-08-19 10:44:50 +0300

    Added Coffee class.

---
+++ C-Features/Coffee.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="Program.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace C_Features
+{
+    internal class Coffee
+    {
+        public Coffee()
+        {
+
+        }
+        public Coffee(string type)
+        {
+            this.CoffeeType = type;
+        }
+        public string CoffeeType { get; set; }
+    }
+}
--- C-Features/Program.cs
+++ C-Features/Program.cs
@@ -21,6 +21,32 @@ namespace C_Features
             var expectedOutput = readText.Result;
             var teamMemberDeserialized = JsonSerializer.Deserialize<TeamMember>(expectedOutput);
             Console.WriteLine(teamMemberDeserialized);
+
+            Coffee coffee = MakeCoffee("grains", "milk", "water", "sugar", Espresso);
+            Console.WriteLine($"Here is your coffee: {coffee}");
+        }
+
+        static Coffee MakeCoffee(string grains, string milk, string water, string sugar, Func<string, string, string, string, Coffee> recipe)
+        {
+            try
+            {
+                Console.WriteLine("Start preparing coffee.");
+                var coffee = recipe(grains, milk, water, sugar);
+                return coffee;
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                Console.WriteLine("Finished.");
+            }
+        }
+
+        static Coffee Espresso(string grains, string milk, string water, string sugar)
+        {
+            return new Coffee("Espresso");
         }
     }
 }
