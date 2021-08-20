abc7ce53d3a0df457ca3d63029ee3dc8639f1b57
AzureAD\SorinaKupan <sorina.kupan@principal33.com>
2021-08-20 10:55:27 +0300

    Added MessageHub. Added AddSignalR  and endpoint in Startup.

--- HelloWorldWeb/Startup.cs
+++ HelloWorldWeb/Startup.cs
@@ -47,6 +47,8 @@ namespace HelloWorldWeb
                 var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
                 c.IncludeXmlComments(xmlPath, includeControllerXmlComments: true);
             });
+
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -79,6 +81,7 @@ namespace HelloWorldWeb
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapHub<MessageHub>("/messagehub");
             });
         }
     }
