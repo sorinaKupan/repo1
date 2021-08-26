4a2b227c7f2e380a30156ae8f195f558a4822b93
AzureAD\SorinaKupan <sorina.kupan@principal33.com>
2021-08-26 10:38:15 +0300

    Added Authorize[Roles=...] to SkillsController.

--- HelloWorldWeb/Controllers/SkillsController.cs
+++ HelloWorldWeb/Controllers/SkillsController.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace HelloWorldWeb.Controllers
 {
-    [Authorize]
+    [Authorize(Roles = "Administrators")]
     public class SkillsController : Controller
     {
         private readonly ApplicationDbContext context;
